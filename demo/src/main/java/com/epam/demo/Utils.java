9d178531ab0290a0e6f57febf51b6544d3c79c1a
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-16 17:41:53 +0500

    Test

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -8,13 +8,9 @@ public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         for (String s : args) {
             if (!StringUtils.isPositiveNumber(s)) {
-             return false;
+                return false;
             }
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(isAllPositiveNumbers(List.of("1", "2", "3", "0")));
     }
 }
