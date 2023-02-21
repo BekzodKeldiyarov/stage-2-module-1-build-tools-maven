377554002b7ee5d363cab6c7cb5fd6a79db12d7c
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-22 00:54:31 +0500

    Done

--- demo/src/main/java/com/epam/demo/Main.java
+++ demo/src/main/java/com/epam/demo/Main.java
@@ -3,12 +3,12 @@ package com.epam.demo;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Main main = new Main();
-        main.testNumbers(List.of("1", "2", "3", "4"));
-    }
-
-    public boolean testNumbers(List<String> numbers) {
-        return Utils.isAllPositiveNumbers(numbers);
-    }
+//    public static void main(String[] args) {
+//        Main main = new Main();
+//        System.out.println(main.testNumbers(List.of("1", "2", "3", "4")));
+//    }
+//
+//    public boolean testNumbers(List<String> numbers) {
+//        return Utils.isAllPositiveNumbers(numbers);
+//    }
 }
