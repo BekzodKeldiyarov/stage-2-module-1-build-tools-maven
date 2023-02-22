5b97da3c2544f9043cdd75d67af62ae61efbc324
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-22 18:08:17 +0500

    Added compiler plugin and maven jar plugin

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -5,9 +5,10 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (NumberUtils.toInt(str) <= 0) {
-            return false;
-        }
-        return true;
+        return  NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
+//        if (NumberUtils.toInt(str) <= 0) {
+//            return false;
+//        }
+//        return true;
     }
 }
