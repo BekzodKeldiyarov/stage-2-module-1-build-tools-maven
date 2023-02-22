ff5ab87152f9d29adbf6fc91a7a334379edc7e6d
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-22 17:38:16 +0500

    Test

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.*;
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (NumberUtils.toInt(str) <= 0) {
+            return false;
+        }
+        return true;
     }
 }
